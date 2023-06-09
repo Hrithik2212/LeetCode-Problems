0581a99b25a182258c7674fdc1db675e1c2a30ae
Hrithik2212 <hr6466@srmist.edu.in>
2023-06-09 09:35:39 +0530

    HashMap - Time Complexity : O(N)

---
+++ Array and Hashing/ContainDuplicate.java
@@ -0,0 +1,42 @@
+/*
+217. Contains Duplicate
+Easy
+9.5K
+1.2K
+Companies
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,1]
+Output: true
+Example 2:
+
+Input: nums = [1,2,3,4]
+Output: false
+Example 3:
+
+Input: nums = [1,1,1,3,3,4,3,2,4,2]
+Output: true
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+-109 <= nums[i] <= 109
+*/ 
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Map <Integer , Integer> map =new HashMap<Integer , Integer>() ; 
+        for(int i = 0 ; i < nums.length ; i++){
+            if (map.containsKey(nums[i])){
+                return true ;
+            }
+            map.put(nums[i],i);
+        }
+    return false;
+    }
+}
