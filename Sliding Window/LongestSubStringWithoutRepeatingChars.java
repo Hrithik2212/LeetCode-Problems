ea48c19d0d3615bbb1d11668fa68e1fe91097977
Hrithik2212 <hr6466@srmist.edu.in>
2023-07-27 09:59:45 +0530

    HashSet - TC : O(N)

---
+++ Sliding Window/LongestSubStringWithoutRepeatingChars.java
@@ -0,0 +1,40 @@
+/*
+Given a string s, find the length of the longest 
+substring
+ without repeating characters.
+
+ 
+
+Example 1:
+
+Input: s = "abcabcbb"
+Output: 3
+Explanation: The answer is "abc", with the length of 3.
+Example 2:
+
+Input: s = "bbbbb"
+Output: 1
+Explanation: The answer is "b", with the length of 1.
+Example 3:
+
+Input: s = "pwwkew"
+Output: 3
+Explanation: The answer is "wke", with the length of 3.
+Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+*/
+
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int left = 0 , maxLength = 0;
+        HashSet<Character> set = new HashSet<>() ; 
+        for(int right=0;right<s.length();right++){
+            while(set.contains(s.charAt(right))){
+                set.remove(s.charAt(left));
+                left++;
+            }
+            set.add(s.charAt(right));
+            maxLength = Math.max(maxLength , right - left +1);
+        }
+        return maxLength;
+    }
+}
