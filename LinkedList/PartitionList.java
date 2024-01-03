cdc3835d49290a1218ffb00f64001d7197b23a36
Hrithik2212 <hr6466@srmist.edu.in>
2024-01-03 11:24:54 +0530

    Merging Lists - TC : O(N)

---
+++ LinkedList/PartitionList.java
@@ -0,0 +1,55 @@
+// Given the head of a linked list and a value x, partition it such that all nodes less than x come before nodes greater than or equal to x.
+
+// You should preserve the original relative order of the nodes in each of the two partitions.
+
+ 
+
+// Example 1:
+
+
+// Input: head = [1,4,3,2,5,2], x = 3
+// Output: [1,2,2,4,3,5]
+// Example 2:
+
+// Input: head = [2,1], x = 2
+// Output: [1,2]
+ 
+
+// Constraints:
+
+// The number of nodes in the list is in the range [0, 200].
+// -100 <= Node.val <= 100
+// -200 <= x <= 200
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode partition(ListNode head, int x) {
+        if(head == null) return null ; 
+        ListNode leftDummyHead = new ListNode() ; 
+        ListNode rightDummyHead = new ListNode() ; 
+        ListNode left = leftDummyHead , right = rightDummyHead ; 
+        while(head != null ){
+            if(head.val < x ) {
+                left.next = head ; 
+                left = left.next ; 
+            }
+            else {
+                right.next = head ; 
+                right = right.next ; 
+            }
+            head = head.next ; 
+        }
+        left.next = rightDummyHead.next ; 
+        right.next = null ; 
+        return leftDummyHead.next ; 
+    }
+}
